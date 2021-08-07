c823de91a228a3905b9b4f8a3acdf16602cc8d42
Pranav <reachpv05@gmail.com>
2021-08-06 21:05:34 -0700

    test.py scratch commit

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 ####Just a scratch file to test out stuff
-'''
 import nltk
 nltk.download('punkt')
 
@@ -7,7 +6,4 @@ from newspaper import Article
 url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
 article = Article(url)
 
-article.download()
-article.parse()
-article.nlp()
-'''
+article.text()
