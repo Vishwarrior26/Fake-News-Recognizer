edec7eaa953bf6a8033c307f931f815f9e39a3c6
Pranav <reachpv05@gmail.com>
2021-08-07 12:34:06 -0700

    Added machine learning functionality

--- app.py
+++ app.py
@@ -26,10 +26,10 @@ def model_input(input_text):
     training_size = 20000
 
     with open('train_text.txt') as file:
-        train_text = file.readlines()
+        train_texts = file.readlines()
         file.close()
     with open('test_text.txt') as file:
-        test_text = file.readlines()
+        test_texts = file.readlines()
         file.close()
     with open('train_labels.txt') as file:
         train_labels = file.readlines()
@@ -37,18 +37,18 @@ def model_input(input_text):
     with open('test_labels.txt') as file:
         test_labels = file.readlines()
         file.close()
-    print(len(train_text))
+    print(len(train_texts))
     tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
-    tokenizer.fit_on_texts(train_text)
+    tokenizer.fit_on_texts(train_texts)
 
     word_index = tokenizer.word_index
 
-    train_sequences = tokenizer.texts_to_sequences(train_text)
+    train_sequences = tokenizer.texts_to_sequences(train_texts)
     train_padded = pad_sequences(train_sequences, maxlen=max_length,
                                  padding=padding_type,
                                  truncating=trunc_type)
 
-    test_sequences = tokenizer.texts_to_sequences(test_text)
+    test_sequences = tokenizer.texts_to_sequences(test_texts)
     test_padded = pad_sequences(test_sequences, maxlen=max_length,
                                 padding=padding_type,
                                 truncating=trunc_type)
@@ -58,14 +58,25 @@ def model_input(input_text):
     test_padded = np.array(test_padded)
     test_labels = np.array(test_labels)
 
+    import numpy as np
+    import tensorflow as tf
+    train_padded = np.array(train_padded)
+    train_labels = np.array(train_labels)
+    test_padded = np.array(test_padded)
+    test_labels = np.array(test_labels)
+
+
     model = tf.keras.Sequential([
-        tf.keras.layers.Embedding(
-            vocab_size, embedding_dim, input_length=max_length),
+        tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
         tf.keras.layers.GlobalAveragePooling1D(),
         tf.keras.layers.Dense(20, activation='relu'),
         tf.keras.layers.Dropout(0.5),
+        #tf.keras.layers.Dense(20, activation='relu'),
+        #tf.keras.layers.Dropout(0.5),
         tf.keras.layers.Dense(1, activation='sigmoid')
     ])
+    model.compile(loss='binary_crossentropy',optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001), metrics=['accuracy'])
+
     text = [input_text]
     sequences = tokenizer.texts_to_sequences(text)
     padded = pad_sequences(sequences, maxlen=max_length,
@@ -86,11 +97,11 @@ def home():
 @app.route("/result", methods=['POST'])
 def result():
     news_link = request.form['news_link']
-    url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
-    article = Article(url)
-    input_text = article.title + " " + article.text
-    model_input(input_text)
-    return render_template("result.html")
+    #url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
+    article = Article(news_link)
+    input_text = article.text
+    prediction = model_input(input_text)
+    return render_template("result.html", model_prediction = prediction)
 
 
 if __name__ == "__main__":
