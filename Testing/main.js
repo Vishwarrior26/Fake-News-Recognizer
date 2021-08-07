ecbd9df6ff198ef2b0a6b641e0e1b4cb3508807f
Vishwarrior26 <67435125+Vishwarrior26@users.noreply.github.com>
2021-08-07 16:28:09 -0700

    Update main.js

--- Testing/main.js
+++ Testing/main.js
@@ -18,14 +18,7 @@ if (index != -1) {
 }
 
 <<<<<<< HEAD
-// console.log(cleanerText);
+
 import * as tf from './tf.js';
 const model = tf.loadLayersModel('model.json');
 console.log("test")
-=======
-console.log(cleanerText);
-
-const model =  tf.loadLayersModel('model.json');
-
-
->>>>>>> e341d19fcae3c6b7708a940a4c09a1ef0d976fa3
