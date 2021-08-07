7974022e600a761db275b4ef4d8f45b381f25e09
Vishwarrior26 <srimathkandali@gmail.com>
2021-08-07 09:36:02 -0700

    Adjusted main.js to handle text cleaning

--- Testing/main.js
+++ Testing/main.js
@@ -2,9 +2,18 @@
 var dirtyText = document.body.innerText;
 var title = document.title;
 var index = dirtyText.indexOf(title);
-
+var cleanerText = "";
 // print results
 console.log(document.title);
-console.log(dirtyText);
-console.log(index);
-console.log(dirtyText.slice(index));
+// console.log(dirtyText);
+// console.log(index);
+// console.log(dirtyText.slice(index));
+
+if (index != -1){
+  cleanerText = dirtyText.slice(index);
+}
+else {
+  cleanerText = dirtyText;
+}
+
+console.log(cleanerText);
