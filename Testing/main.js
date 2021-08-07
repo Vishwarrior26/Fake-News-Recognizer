ab2929be8e2f58c31e6b2aa38e9f346d124cb7db
Vishwarrior26 <srimathkandali@gmail.com>
2021-08-07 16:40:19 -0700

    Alerting methods of tf

--- Testing/main.js
+++ Testing/main.js
@@ -14,7 +14,21 @@ if (index != -1) {
   cleanerText = dirtyText;
 }
 
-// console.log(cleanerText);
 import * as tf from './tf.js';
-const model = await tf.loadLayersModel('./model.json');
-console.log("test")
+
+function getMethods(obj) {
+  var result = [];
+  for (var id in obj) {
+    try {
+      if (typeof(obj[id]) == "function") {
+        result.push(id + ": " + obj[id].toString());
+      }
+    } catch (err) {
+      result.push(id + ": inaccessible");
+    }
+  }
+  return result;
+}
+
+alert(getMethods(tf).join("\n"));
+// const model = await tf.loadLayersModel('./model.json');
