0b5d19e4be652dc641f5c5f6911a6bc94c9e315e
Vishwarrior26 <srimathkandali@gmail.com>
2021-08-06 22:27:17 -0700

    Prints the Title, Dirty Text, Index of Title in Dirty Text, and Cleaner Text(Dirty Text after Title)

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 var dirtyText = document.body.innerText
+var title = document.title
+var index = dirtyText.indexOf(title)
 console.log(document.title)
 console.log(dirtyText)
+console.log(index)
+console.log(dirtyText.slice(index))
