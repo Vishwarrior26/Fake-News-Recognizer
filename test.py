0faf52cf626646d167738b38a73e9db909e091c7
Pranav <reachpv05@gmail.com>
2021-08-06 20:28:55 -0700

    commited test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+####Just a scratch file to test out stuff
+'''
+import nltk
+nltk.download('punkt')
+
+from newspaper import Article
+url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
+article = Article(url)
+
+article.download()
+article.parse()
+article.nlp()
+'''
