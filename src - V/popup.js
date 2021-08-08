60b574ff3d27c5319a3cd33452a3320846085f51
Vishwarrior26 <srimathkandali@gmail.com>
2021-08-07 23:23:15 -0700

    Minor additions

--- src - V/popup.js
+++ src - V/popup.js
@@ -1,6 +1,13 @@
 document.getElementById('button').addEventListener('click', fakeOrNot);
 
+function delay(n) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, n * 1000);
+  });
+}
+
 function fakeOrNot() {
   //TODO API stuff here
   document.getElementById("answer").innerHTML = "processing...";
+  document.getElementById("percentage").innerHTML = "0%";
 }
