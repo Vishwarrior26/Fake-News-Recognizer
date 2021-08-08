f2deef3b0b001b82dba9dddc5570e24f62c6af10
Vishwarrior26 <srimathkandali@gmail.com>
2021-08-07 22:05:46 -0700

    Added helper files

--- src - V/background.js
+++ src - V/background.js
@@ -1,7 +1,6 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-   chrome.tabs.executeScript(null, {file: "main.js"});
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    files: ['main.js']
+  });
 });
