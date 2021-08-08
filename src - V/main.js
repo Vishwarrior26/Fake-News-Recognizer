25a6d39bcb74bd5e8e5f7f802d9c0b147b82dc82
Vishwarrior26 <srimathkandali@gmail.com>
2021-08-07 22:40:57 -0700

    Fixed issue with extension not working

--- src - V/main.js
+++ src - V/main.js
@@ -1,10 +1,14 @@
+
 var dirtyText = document.body.innerText;
-var index = dirtyText.indexOf(title);
+var index = dirtyText.indexOf(document.title);
 var cleanerText = "";
 
-if (index != -1) {
-  cleanerText = dirtyText.slice(index);
-} else {
-  cleanerText = dirtyText;
-}
-cleanerText = cleanerText.replace(/\s+/g, '-').toLowerCase();
+// if (index != -1) {
+//   cleanerText = dirtyText.slice(index);
+// } else {
+//   cleanerText = dirtyText;
+// }
+// cleanerText = cleanerText.replace(/\s+/g, '-').toLowerCase();
+cleanerText = dirtyText.replace(/\s+/g, '-').toLowerCase();
+
+console.log(cleanerText)
